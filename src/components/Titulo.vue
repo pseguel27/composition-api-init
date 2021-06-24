d0407f8b2bc8c3fd1f24483def96dd067531b78d
pseguel27 <pato.seguel.moya@gmail.com>
2021-06-24 02:34:22 -0400

    step4 props

---
+++ src/components/Titulo.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1
+   :style="{'color': color}">
+    Contador: {{signoPeso}}
+  </h1>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+  props: ['contador', 'color'],
+  setup(props){
+    const signoPeso = computed(()=>{
+      return '$' + props.contador;
+    })
+    return {signoPeso}
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="about">
-   <h1
-    :style="{'color': color}">
-      Contador: {{contador}}
-    </h1>
+    <Titulo :contador="contador" :color="color" />
+    <!--<h1
+      :style="{'color': color}">
+        Contador: {{contador}}
+    </h1>-->
     <button @click="aumentar">+</button>
     <button @click="disminuir">-</button>
     <hr>
@@ -13,8 +14,12 @@
 </template>
 
 <script>
+import Titulo from '../components/Titulo'
 import { computed, ref } from 'vue'
 export default {
+  components: {
+    Titulo
+  },
   //se ejecuta antes que se cree el componente, una vez que los props se resuelven, y es el punto de entrada para las API de composición
   //this desaparece, acceso directo a todos los datos
   setup() {
